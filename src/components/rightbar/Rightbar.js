adc2be08dfd097741ef8a4d1156def3dcbaa5a5c
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-08-05 20:05:27 +0530

    RightBar commit

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" className="birthdayImg" />
+          <span className="birthdaytext">
+            <b>Vivek Pandey</b> and <b>4 other friend</b> have birthday today
+          </span>
+        </div>
+        <img src="assets/ads.png" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+      </div>
+    </div>
+  );
 }
